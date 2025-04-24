e30ad0f7505a21d666ba75365491d3acbc7814dc
li <86910127+i-rick-y@users.noreply.github.com>
2025-04-25 02:29:43 +0300

    something

--- Olive_VPN_app/app/Components/Pages/ServersList/MainContent/MainContent.jsx
+++ Olive_VPN_app/app/Components/Pages/ServersList/MainContent/MainContent.jsx
@@ -18,38 +18,35 @@ const MainContent = () => {
   const [styles, theme] = useThemes (styles => styles.ServersListPage.Main)
   const [texts] = useAppLanguage (texts => texts.ServersListPage.Main)
 
-  const [netherlandsAvailability, setNetherlandsAvailability] = useState ()
-  const [germanyAvailability, setGermanyAvailability] = useState ()
-  const [finlandAvailability, setFinlandAvailability] = useState ()
+  const [netherlandsAvailability, setNetherlandsAvailability] = useState (null)
+  const [germanyAvailability, setGermanyAvailability] = useState (null)
+  const [finlandAvailability, setFinlandAvailability] = useState (null)
 
   useEffect (() => {
 
     const checkStatuses = async () => {
 
       let netherlands_data
-      try {
-        netherlands_data = await fetch ('http://77.83.86.57:505/get_server_load_data')
-        netherlands_data = await netherlands_data.json ()
-      }
-      catch (error) {netherlands_data = null}
+
+      netherlands_data = await fetch ('http://77.83.86.57:505/get_server_load_data')
+      netherlands_data = await netherlands_data.json ()
+
 
       let germany_data
-      try {
-        germany_data = await fetch ('http://217.11.166.234:505/get_server_load_data')
-        germany_data = await germany_data.json ()
-      }
-      catch (error) {germany_data = null}
+
+      germany_data = await fetch ('http://217.11.166.234:505/get_server_load_data')
+      germany_data = await germany_data.json ()
+
 
       let finland_data
-      try {
-        finland_data = await fetch ('http://217.11.167.238:505/get_server_load_data')
-        finland_data = await finland_data.json ()
-      }
-      catch (error) {finland_data = null}
 
-      netherlands_data != null && setNetherlandsAvailability (parseFloat (netherlands_data))
-      germany_data != null && setGermanyAvailability (parseFloat (germany_data))
-      finland_data != null && setFinlandAvailability (parseFloat (finland_data))
+      finland_data = await fetch ('http://217.11.167.238:505/get_server_load_data')
+      finland_data = await finland_data.json ()
+
+
+      typeof netherlands_data == 'number' && setNetherlandsAvailability (parseFloat (netherlands_data))
+      typeof germany_data == 'number' && setGermanyAvailability (parseFloat (germany_data))
+      typeof finland_data == 'number' && setFinlandAvailability (parseFloat (finland_data))
 
     }
 
