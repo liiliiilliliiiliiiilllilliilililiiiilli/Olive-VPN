8172f0551ccc6eef300d6929dcc8405f6cd7e2fb
li <86910127+i-rick-y@users.noreply.github.com>
2025-04-28 20:17:38 +0300

    q

--- Olive_VPN_app/app/Components/Pages/ServersList/MainContent/MainContent.jsx
+++ Olive_VPN_app/app/Components/Pages/ServersList/MainContent/MainContent.jsx
@@ -40,7 +40,7 @@ const MainContent = () => {
 
 
         axios.get ('http://217.11.167.238:505/get_server_load_data')
-        .then (res => setFinlandAvailability (0), error => {})
+        .then (res => setFinlandAvailability (parseFloat (0)), error => {})
 
       ])
 
